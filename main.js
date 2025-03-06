64d507afdb8f5549eee33bac80fe7cc1fcb4cd9b
unknown <natalia.pineda2024@hilasal.superate.org.sv>
2025-03-06 11:09:39 -0600

    Final

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // conexión con html (seleccionar elementos)
- 
+
 const songForm = document.getElementById('song_form');
 const playList = document.getElementById('play_list');
- 
+
 // Función para agregar una canción a la lista
- 
+
 function addSong(songName, artistName, songUrl){
     const listSong = document.createElement('li');
     listSong.innerHTML = `
@@ -15,24 +15,28 @@ function addSong(songName, artistName, songUrl){
     `;
     // Agregar los cambios al html
     playList.appendChild(listSong);
- 
- 
+
+    // Eliminar la canción
+    const deleteButton = listSong.querySelector('.delete-btn');
+    deleteButton.addEventListener('click', () => {
+        listSong.remove(); 
+        console.log('Canción eliminada');
+    });
+
 }
- 
+
 // manejo de envios de formularios (Eventos)
- 
+
 songForm.addEventListener('submit', (event) => {
     event.preventDefault();
- 
+
     //Obtener formulario
     const songName = document.getElementById("song_name").value;
     const artistName = document.getElementById("artist_name").value;
     const songUrl = document.getElementById("url_song").value;
- 
+
     addSong(songName, artistName, songUrl);
- 
+
     songForm.reset();
-   
- 
- 
+
 } )
